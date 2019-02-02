77dba30f8d47b78ebbc7d5082252fa72d0bc71a6
chent <emailctao@163.com>
2019-02-02 16:57:24 +0800

    up

--- config.go
+++ config.go
@@ -20,11 +20,30 @@ func LoadConfig(path string) {
 		return
 	}
 
-	cfg.Section("app").MapTo(&Config.App)
-	cfg.Section("log").MapTo(&Config.Log)
-	cfg.Section("server").MapTo(&Config.Server)
-	cfg.Section("database").MapTo(&Config.Db)
-	cfg.Section("template").MapTo(&Config.Tpl)
+	if err := cfg.Section("app").MapTo(&Config.App); err != nil {
+		Debug("app config not found")
+		return
+	}
+
+	if err := cfg.Section("log").MapTo(&Config.Log); err != nil {
+		Debug("log config not found")
+		return
+	}
+
+	if err := cfg.Section("server").MapTo(&Config.Server); err != nil {
+		Debug("server config not found")
+		return
+	}
+
+	if err := cfg.Section("database").MapTo(&Config.Db); err != nil {
+		Debug("database config not found")
+		return
+	}
+
+	if err := cfg.Section("template").MapTo(&Config.Tpl); err != nil {
+		Debug("template config not found")
+		return
+	}
 
 	return
 }
