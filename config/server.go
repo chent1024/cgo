be16143033c40ac63456fc0ae0e40fc5bc274fc3
chent <emailctao@163.com>
2019-02-11 14:47:11 +0800

    gofmt

--- config/server.go
+++ config/server.go
@@ -1,8 +1,8 @@
 package config
 
 type ServerConfig struct {
-	Address string `ini:"address"`
-	ReadTimeout  int `ini:"read_timeout"`
-	WriteTimeout int `ini:"write_timeout"`
-	IdleTimeout  int `ini:"idle_timeout"`
+	Address      string `ini:"address"`
+	ReadTimeout  int    `ini:"read_timeout"`
+	WriteTimeout int    `ini:"write_timeout"`
+	IdleTimeout  int    `ini:"idle_timeout"`
 }
